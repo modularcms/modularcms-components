d8724fad82b150b5ebe12ce029808d786e7f66e1
Felix Bröhl <broehl@glamus.de>
2020-06-11 14:50:08 +0200

    Change input css to globalized one

---
+++ page_manager/ccm.page_manager.js
@@ -0,0 +1,72 @@
+/**
+ * @overview modularcms component that manages the pages
+ * @author Felix Bröhl <broehl@everoo.io> 2020
+ * @license The MIT License (MIT)
+ */
+
+( () => {
+
+    const component = {
+
+        name: 'page_manager',
+
+        ccm: 'https://ccmjs.github.io/ccm/versions/ccm-25.5.3.js',
+
+        config: {
+            "html": [ "ccm.load", "https://modularcms.github.io/modularcms-components/page_manager/resources/html/page_manager.html" ],
+            "css": [ "ccm.load",
+                "https://modularcms.github.io/modularcms-components/page_manager/resources/css/style.css",
+                "https://modularcms.github.io/modularcms-components/cms/resources/css/inputs.css"
+            ],
+            "helper": [ "ccm.load", "https://ccmjs.github.io/akless-components/modules/versions/helper-5.1.0.mjs" ],
+            "pages": [ "ccm.store", { "name": "fbroeh2s_pages", "url": "https://ccm2.inf.h-brs.de" } ],
+        },
+
+        Instance: function () {
+            let $;
+
+            this.ready = async () => {
+                $ = Object.assign( {}, this.ccm.helper, this.helper );                 // set shortcut to help functions
+            };
+
+            /**
+             * Starts the component
+             * @returns {Promise<void>}
+             */
+            this.start = async () => {
+                $.setContent(this.element, $.html(this.html.main, {}));
+                //this.getAllPages();
+            };
+
+            /**
+             * Creates a new page
+             * @returns {Promise<void>}
+             */
+            this.createNewPage = async () => {
+                console.log(this.pages.set({
+                    value: 'test'
+                }));
+            }
+
+            /**
+             * Loads all Pages
+             * @returns {Promise<void>}
+             */
+            this.loadAllPages = async () => {
+                const data = self.store.get();
+                const list = this.element.querySelector('#list');
+
+                /*
+                 * Iterate through all data
+                 */
+                data.forEach(function (element) {
+                    $.append(list, $.html(this.html.listItem, {title: element.title, route: item.route}))
+                });
+            }
+
+        }
+
+    };
+
+    let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||["latest"])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){window.ccm[c].component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+} )();
