bd9a326407af0d78276f422b4bebb3581ecc7429
Felix Bröhl <broehl@glamus.de>
2020-07-12 14:08:52 +0200

    Add cloudinary image builder

---
+++ cloudinary_image_builder/versions/ccm.cloudinary_image_builder-2.1.0.js
@@ -0,0 +1,78 @@
+/**
+ * @overview ccm component for building a cloudinary image
+ * @author Felix Bröhl <broehl@everoo.io> 2018-2020 (originally was forked json_builder from André Kless)
+ * @license The MIT License (MIT)
+ * @version 1.0.0
+ */
+
+( () => {
+
+  const component = {
+
+    name: 'cloudinary_image_builder',
+
+    version: [ 1, 0, 0 ],
+
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-25.5.3.min.js',
+
+    config: {
+      "css": [ "ccm.load", "https://modularcms.github.io/modularcms-components/cloudinary_image_builder/resources/default.css" ],
+      "data": "https://res.cloudinary.com/dyhjqgkca/image/upload/v1594499974/cms/a8xtbzcxelldugfz82i5.svg",
+  //  "directly": true,
+      "helper": [ "ccm.load", "https://ccmjs.github.io/akless-components/modules/versions/helper-4.1.1.mjs" ],
+      "html": [ "ccm.load", "https://modularcms.github.io/modularcms-components/cloudinary_image_builder/resources/templates.html" ]
+    },
+
+    Instance: function () {
+
+      let $, dataset, tmp;
+
+      this.ready = async () => {
+
+        // set shortcut to help functions
+        $ = Object.assign( {}, this.ccm.helper, this.helper );
+
+        // logging of 'ready' event
+        this.logger && this.logger.log( 'ready', $.privatize( this, true ) );
+
+      };
+
+      this.start = async () => {
+        $.setContent(this.element, $.html(this.html.main, {}));
+
+        // Create cloudinary widget
+        const cloudinaryWidget = cloudinary.createUploadWidget(
+            {
+              cloudName: 'dyhjqgkca',
+              uploadPreset: 'y6tm2ylf',
+              sources: ['local'],
+              googleApiKey: 'AIrFcR8hKiRo',
+              multiple: false,
+              resourceType: 'image',
+              palette: {
+                windowBorder: '#000000'
+              }
+            },
+            async (error, result) => {
+              if (!error && result && result.event === "success") {
+                const imgData = result.info;
+                this.data = imgData.secure_url;
+                this.onchange && await this.onchange( { instance: this } );
+              }
+            }
+        );
+        this.element.querySelector('open').addEventListener(() => {
+          cloudinaryWidget.open();
+        })
+      };
+
+      this.isValid = () => true;
+
+      this.getValue = () => this.data;
+
+    }
+
+  };
+
+  let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||["latest"])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){window.ccm[c].component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+} )();
